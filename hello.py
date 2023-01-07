10a4c6e6eb4f393ce489d207d4c4eaa7a0dd191a
ignatflorin <carpentrius.inc@gmail.com>
2023-01-07 11:14:59 -0600

    hello.py is commited

---
+++ hello.py
@@ -0,0 +1,28 @@
+
+#Notes: the line below prints the text with double quotations
+print("Hello World")
+
+print('Hello World')
+
+# what if i want to write or print multiple lines with one print function ?
+# this is the exemple fot multiline function 
+
+print("""Hello
+This is the example for 
+multiline  print function""")
+
+print('''Hello 
+this is the example for multiline 
+in single print function using triple single quotation''')
+
+# i'd like to go watch a movie
+print("i'd like to go watch a movie")
+
+#I'm found of saying "something"
+# when we define the sentence above in a python text 
+#we should tell python whitch quotation it should consider
+#as a python code
+# way to tell python it is in the text is using \ (backslash)
+print("I'm found of saying \"something\" ")
+#how can i define the sentence in a single quoted text
+print('I\'m found of saying "somenthing"')
